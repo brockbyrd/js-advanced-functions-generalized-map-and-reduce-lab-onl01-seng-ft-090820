3ec378ac48a13c57a759190cc527c1c49a51cfd3
brockbyrd <brockbyrdd@gmail.com>
2020-12-23 12:42:54 -0600

    commit tree c578faa34eaeecd4734eb217f30c04363ebd02e8

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+
+function map(src, cb) {
+    return src.map(cb)
+}
+
+function reduce(src, cb, startingPoint){
+    if (!!startingPoint === false){
+        startingPoint = src.shift()
+    }
+    return src.reduce(cb, startingPoint)
+}
